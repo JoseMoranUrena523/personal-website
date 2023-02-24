2c2a260c0076c1900848e43d0ecc696045dc3a45
Jose Moran Urena <jose.moranurena.official@gmail.com>
2023-02-23 20:36:20 -0500

    Update redirect-to-lightning.js

--- functions/redirect-to-lightning.js
+++ functions/redirect-to-lightning.js
@@ -2,7 +2,7 @@ exports.handler = function(event, context, callback) {
   return callback(null, {
     statusCode: 200,
     headers: {
-      'Location': 'https://lightning.josemoranurena.tech/.well-known/lnurlp/jose'
+      'Location': 'https://josemoranurena.eu.org/.well-known/lnurlp/jose'
     },
     body: ''
   });
