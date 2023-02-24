5f1b31639e1bcdf6813713d75fc30ed2ddb1b618
Jose Moran Urena <jose.moranurena.official@gmail.com>
2023-02-23 20:49:05 -0500

    Delete functions directory

--- functions/redirect-to-lightning.js
+++
@@ -1,9 +0,0 @@
-exports.handler = function(event, context, callback) {
-  return callback(null, {
-    statusCode: 200,
-    headers: {
-      'Location': 'https://josemoranurena.eu.org/.well-known/lnurlp/jose'
-    },
-    body: ''
-  });
-};
